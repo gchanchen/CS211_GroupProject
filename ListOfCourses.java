d80c7fac5cb55f869580b65ad1502d6b7f6a3857
ngosm27 <56649705+ngosm27@users.noreply.github.com>
2019-11-16 14:52:14 -0500

    Removed unused methods

--- ListOfCourses.java
+++ ListOfCourses.java
@@ -12,24 +12,18 @@ public class ListOfCourses{
       
    public void addCourse(Course c){
       list.add(c);}
-      
+   
+   //needed?   
    public void removeCourse(Course c){
       list.remove(c);}
-   
-   public Course get(int index){
-      return list.get(index);}
-   
-   public int indexOf(Course c){
-      for(int i = 0; i< list.size(); i++){
-         if (list.get(i).getCourseName().equals(c.getCourseName()))
-            return i;  }
-      return -1;}
       
    public int size(){
       return list.size();}
-      
-   public void display(){
-      for (int i = 0; i < list.size(); i++){
-         System.out.print(list.get(i).getCourseName() + " ");
-         System.out.println(list.get(i).getTakenStatus());}}
+         
+   public String toString(){
+      String str = "";
+      for(Course c : list)
+         str += c.getCourseName() + " [" + c.getCredit() + "]" + "\n";
+         
+      return str;}
    }
