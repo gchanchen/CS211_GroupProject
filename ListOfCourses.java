5f71192b756bb2b0a19b7bd6f970d5434295066d
ngosm27 <56649705+ngosm27@users.noreply.github.com>
2019-11-27 17:27:43 -0500

    Update ListOfCourses.java
    Added a method, contains, whose role is to loop through each course the user has taken and check if they have selected all each course's prerequisites. 
    
    (For example, it is not possible to take CS 310 without taking CS 112, CS 211, and MATH 113. If the user forgets at least one of these, they will be unable to proceed)

--- ListOfCourses.java
+++ ListOfCourses.java
@@ -9,16 +9,27 @@ public class ListOfCourses{
       
    public ArrayList<Course> getList(){
       return list;}      
+   
+   public Course getCourse(int i){
+      return list.get(i);}
       
    public void addCourse(Course c){
       list.add(c);}
    
-   //needed?   
    public void removeCourse(Course c){
       list.remove(c);}
-      
+   
+   public void clear(){
+      list.clear();}
+         
    public int size(){
       return list.size();}
+   
+   public boolean contains(String name){
+      for(Course x : list)
+         if(name.equals(x.getCourseName()))
+            return true;
+      return false;}
          
    public String toString(){
       String str = "";
